55df11d9b88609dbddfc73c872dacca99bf57ec9
sypaik.dev <sypaik.dev@gmail.com>
2023-06-06 01:03:19 +0900

    Refactor: axios 모듈화

--- src/api/Apis.js
+++ src/api/Apis.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://openmarket.weniv.co.kr/';
+const url = 'https://openmarket.weniv.co.kr';
 
 // 인스턴스 생성 - 기본 URL, 요청 헤더
 export const instance = axios.create({
@@ -10,8 +10,12 @@ export const instance = axios.create({
   },
 });
 
-
 export const postUserLogin = async (formData) => {
   const response = await instance.post('/accounts/login/', formData);
   return response.data;
 };
+
+export const getProducts = async () => {
+  const response = await instance.get('/products/');
+  return response.data;
+}
