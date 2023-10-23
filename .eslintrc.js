b7f895bac5cd083a40136e326766572e895f0d33
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-10-24 00:14:39 +0900

    chore: jest 테스트 파일 추가 및 commonJS 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
    env: {
       browser: true,
       es2021: true,
+      node: true,
    },
    extends: [
       'eslint:recommended',
@@ -19,17 +20,35 @@ module.exports = {
             sourceType: 'script',
          },
       },
+      {
+         env: {
+            jest: true,
+         },
+         files: ['__tests__/**/*.{ts,tsx}'],
+         parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            project: './tsconfig.json',
+            tsconfigRootDir: __dirname,
+         },
+      },
    ],
    parser: '@typescript-eslint/parser',
    parserOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
    },
-   plugins: ['@typescript-eslint', 'react', 'cypress'],
+   plugins: ['@typescript-eslint', 'react'],
    rules: {
       indent: ['error', 3],
       quotes: ['error', 'single'],
       semi: ['error', 'always'],
       'react/prop-types': 'off',
+      'import/no-commonjs': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
    },
+   'globals': {
+      '__dirname': true,
+   },
+   
 };
